224e57cfa9135ceb728f399fbb5df04673a6da41
Hanan Tamayev <Hanan Tamayev>
2017-02-26 16:14:30 +0200

    final

--- app/components/Controls.jsx
+++ app/components/Controls.jsx
@@ -19,7 +19,7 @@ var Controls = React.createClass({
             if (countdownStatus === 'started') {
                 return (<button className="button secondary" onClick={this.onStatusChange('paused')}>Paused</button>);
             }
-            else if (countdownStatus === 'paused') {
+            else {
                 return (<button className="button primary" onClick={this.onStatusChange('started')}>Start</button>);
             }
         };
